9b401986e95fa7c053d966edf57993e9c643ff88
Bom1187 <bompiputtanachi@gmail.com>
2025-01-21 17:09:09 +0700

    v.2 Finish workshop

--- src/com/sau/workshop/AreaProject02.java
+++ src/com/sau/workshop/AreaProject02.java
@@ -1,17 +1,17 @@
 package com.sau.workshop;
 
 import java.text.DecimalFormat;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class AreaProject02 {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         Double Menu, Width, Long, AreaOfSquare,Base,High,Radius,AreaOfCircle,AreaOfTriangle;
 
-        Scanner sc = new Scanner(System.in);
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
         DecimalFormat df = new DecimalFormat("0.00");
-
-
-
 
         System.out.println("***************************");
         System.out.println("  Shape Area Program  ");
@@ -21,31 +21,31 @@ public class AreaProject02 {
         System.out.println("3.Circle Area");
         System.out.println("***************************");
         System.out.print("Select Menu : " );
-        Menu = sc.nextDouble();
+        Menu = Double.parseDouble( reader.readLine() );
 
 
         if ( Menu == 1 ){
             System.out.println("***************************");
             System.out.print("Inout widths : ");
-            Width = sc.nextDouble();
+            Width = Double.parseDouble( reader.readLine() );
             System.out.print("Input longs : ");
-            Long = sc.nextDouble();
+            Long = Double.parseDouble( reader.readLine() );
             AreaOfSquare = Long * Width;
             System.out.println("Area Of Square is : "+ AreaOfSquare);
             System.out.println("***************************");
         }else if ( Menu == 2 ) {
             System.out.println("***************************");
             System.out.print("Input bases : ");
-            Base = sc.nextDouble();
+            Base  = Double.parseDouble( reader.readLine() );
             System.out.print("Input highs : ");
-            High  = sc.nextDouble();
+            High  = Double.parseDouble( reader.readLine() );
             AreaOfTriangle = Base * High / 2;
             System.out.println("Area of Triangle : " + AreaOfTriangle );
             System.out.println("***************************");
         }else if ( Menu == 3 ) {
             System.out.println("***************************");
             System.out.print("Input raduis : ");
-            Radius = sc.nextDouble();
+            Radius = Double.parseDouble( reader.readLine() );
             AreaOfCircle = 3.14 * (Radius * Radius);
             System.out.println("Area of Circle is : " + AreaOfCircle);
             System.out.println("***************************");
